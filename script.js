861594bce270f5429b01bb363bdcdde418d3a04c
Sirleeem <sirleeem101@gmail.com>
2024-11-11 21:05:18 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+/* Style your page here */
+body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+    background: linear-gradient(135deg, #f9a8d4, #feca57);
+    color: #333;
+    font-family: Arial, sans-serif;
+}
+/* Add more styles as needed */
